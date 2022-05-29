c0173acf8565eb65eb18ff316dd8cd65689a3b60
Roman Geathers <romangeathersdev@gmail.com>
2022-05-29 12:05:43 -0400

    Write "operate" function
    Write function called "operate" to perform operations on 2 numbers
    based on given operation.

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(op, num1, num2) {
+    if (op == "+") {
+        return add(num1, num2);
+    }
+    else if (op == "-") {
+        return subtract(num1, num2);
+    }
+    else if (op == "*") {
+        return multiply(num1, num2);
+    }
+    else if (op == "/") {
+        return divide(num1, num2);
+    }
+}
