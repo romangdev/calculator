93d656ecf892d4eb02b3253d8ce0c0300c95601c
Roman Geathers <romangeathersdev@gmail.com>
2022-05-30 11:17:35 -0400

    Limit displayed results to 4 decimal places

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ opButtons.forEach((button) => {
 // When equals button is clicked, operate on the user input numbers
 equalsbutton.addEventListener("click", () => {
     let result = operate(formerOperator, displayValue, getDisplayValue());
-    display.textContent = `${result}`;
+    display.textContent = `${Number((result).toFixed(4))}`;
     displayValue = null;
     clickedEquals = true;
 });
